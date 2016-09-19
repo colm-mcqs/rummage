503620f82fea224ec4f59c7e7558a9a08b6997fd
Colm O'Shea <colmose@gmail.com>
2016-09-19 21:27:31 +0100

    Added replace functionality

--- rummage.js
+++ rummage.js
@@ -1,33 +1,40 @@
 "use strict";
 var pino = require('pino');
 var pretty = pino.pretty();
+var path = require('path');
 pretty.pipe(process.stdout);
 var log = pino({
     name: 'app',
     safe: true
 }, pretty);
 
+var replace = require('replacestream');
 var fs = require('fs');
 var optimist = require('optimist');
 var recursiveReaddir = require('recursive-readdir');
 function rummage (opts, stream){
-    log.info('rummaging....');
+    log.info(opts, 'rummaging....');
     stream = stream || process.stdout;
-    return new Rummage(stream, opts.match, opts.d, opts.f);
+    return new Rummage(stream, opts.match, opts.t, opts.d, opts.f);
 }
 
-function Rummage (stream, match, dir, filetypes){
+function Rummage (stream, match, transformer, dir, filetypes){
     this.stream = stream;
-    this.dir = dir;
+    this.dir = path.normalize(dir);
     this.match = match;
     this.filetypes = filetypes.map(x => `*.${x}`);
 
+    var that = this;
     this.checkInside = function(match, contents, file){
         if (match.test(contents)) {
-            this.stream.write(file);
+            if(typeof transformer == 'function'){
+                fs.createReadStream(path.join(__dirname, file))
+                    .pipe(replace(match, transformer))
+                    .pipe(fs.createWriteStream(file+'.new'));
+            }
+            that.stream.write(file);
         }
     };
-    var that = this;
     recursiveReaddir(this.dir, this.filetypes, (err, files) => {
         files.forEach(function(file) {
             fs.readFile(file, 'utf-8', function(err, contents) {
@@ -35,36 +42,6 @@ function Rummage (stream, match, dir, filetypes){
             });
         })
     });
-
-    process.on('exit', (evt) => {
-        console.log(evt);
-    });
-}
-
-/*
-Rummage.prototype.write = function(s){
-    return this.stream.write(s);
-};*/
-
-if (require.main === module) {
-    if (arg('-h') || arg('--help')) {
-        usage().pipe(process.stdout)
-    } else if (arg('-v') || arg('--version')) {
-        console.log(require('./package.json').version)
-    } else {
-        process.stdin.pipe(
-            rummage(optimist.argv
-        )).pipe(process.stdout)
-    }
-}
-
-function usage () {
-    return require('fs')
-        .createReadStream(require('path').join(__dirname, 'usage.txt'))
-}
-
-function arg (s) {
-    return !!~process.argv.indexOf(s)
 }
 
 module.exports = rummage;
--- test/rummage.test.js
+++ test/rummage.test.js
@@ -4,7 +4,11 @@ var should = require('should');
 
 describe('rummage', () => {
     it('should rummage', done =>{
-        rummage({match: /return callback\((.*)\)/, d: './test/src', f: ['html', 'css']}, {
+        function transformer (source){
+            return arguments[1]+'new Error('+arguments[2]+')'+arguments[3];
+        }
+
+        rummage({match: /(return callback\()(.*)(\))/, t: transformer, d: './test/src', f: ['html', 'css']}, {
             write: function(res){
                 should.exist(res);
                 res.should.eql('test\\src\\srcchild\\srcchildchild\\child2.js');
